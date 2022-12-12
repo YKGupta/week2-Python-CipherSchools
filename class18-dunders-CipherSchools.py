44c89e83fdf556b357c04066ce96a2e3e494bf65
Yash Kumar Gupta <ykgupta2411@gmail.com>
2022-12-12 22:36:57 +0530

    Class 18

---
+++ class18-dunders-CipherSchools.py
@@ -0,0 +1,23 @@
+class Person:
+    def __init__(self , name):
+        self.name = name
+
+    def say_hi(self):
+        print('Hemlo, my name is:' , self.name)
+
+p = Person('Yash Kumar Gupta') 
+p.say_hi()
+
+a = 1
+b = (a+1)
+print(b)
+del a
+a = 1
+type(a)
+class A:
+    a = 1
+    b = 2
+    def __add__(self, x):
+        return self.a + self.b+x
+a = A()
+print(a.__add__(10))
