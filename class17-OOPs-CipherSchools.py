d93a88d410a381c82b634b1306876f64b82af883
Yash Kumar Gupta <ykgupta2411@gmail.com>
2022-12-12 22:36:42 +0530

    Class 17

---
+++ class17-OOPs-CipherSchools.py
@@ -0,0 +1,14 @@
+#object oriented programme
+student1 = {"name":"Yash","marks":90}
+student2 = {"name":"Kumar","marks":90}
+print(student1["name"])
+
+class Person:
+    pass
+
+p = Person()
+print(p)
+
+a =1 
+def square(a):
+    print(a**2)
